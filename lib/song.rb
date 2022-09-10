840619bc2ff83087461e98cb9186bee406d7a8bb
Melissa Vaiden <melissavaiden@gmail.com>
2022-09-10 12:31:46 -0400

    Completed

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+require 'pry'
+
+class Song
+    attr_accessor :name
+    attr_accessor :artist
+    attr_accessor :genre
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def initialize name, artist, genre
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << self.genre
+        @@artists << self.artist
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end 
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+end
+
